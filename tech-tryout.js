0582bc0e6f8aaca2114ef5f1460bcfdcef946aef
akbaradam24 <adam.akhbar24@gmail.com>
2022-03-16 21:30:24 +0700

    tryout

--- tech-tryout.js
+++ tech-tryout.js
@@ -1,35 +1,24 @@
-/*
-create function to determine the given expression is correct or not
-expression will be string of open bracket nor close bracket e.g ({[[()]]})
-your function should determine the expression is it correct or not
-examples:
-({[]}) => true
-([][]{})=> true
-({)(]){[} => false
-[)()] => false
-*/
-
-function isExpression(string) {
-    let stack = []
-    let obj = {
-        '{': '}',
-        '[': ']',
-        '(': ')'
+function letSee(string) {
+  let stack = [];
+  let obj = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == "[" || string[i] == "{" || string[i] == "(") {
+      stack.push(string[i]);
+    } else {
+      let answ = stack.pop();
+      if (string[i] != obj[answ]) {
+        return false;
+      }
     }
-    for(let i = 0; i < string.length; i++) {
-        if(string[i] == '[' || string[i] == '{' || string[i] == '(') {
-            stack.push(string[i])
-        } else {
-            let answ = stack.pop()
-            if(string[i] != obj[answ]) {
-                return false
-            }
-        }
-    }
+  }
 
-    return stack.length == 0
+  return stack.length == 0;
 }
 
-let string = '{[)](}'
+let string = "{[)](}";
 
-console.log(isExpression(string))
+console.log(letSee(string));
